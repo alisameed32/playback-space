9e416ac188c50a7683fece7f3cc8105514dd572f
Ali Sameed <alisameed.cs@gmail.com>
2026-01-23 16:13:10 +0500

    feat: add healthcheck endpoint with controller and route

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,8 +25,10 @@ import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 // routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/playlist", playlistRouter);
@@ -34,5 +36,6 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
 
 export { app };
