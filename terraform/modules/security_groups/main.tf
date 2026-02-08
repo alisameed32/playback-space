ce511823ea652680cd07e3c7661d1238039156f0
Ali Sameed <alisameed.cs@gmail.com>
2026-02-09 00:56:01 +0500

    feat(terraform): add additional ingress rules for security group to support SMTP and Kubernetes traffic

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -23,5 +23,45 @@ resource "aws_security_group" "main" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "kubernetes node port range"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Secure SMTP port for email notifications"
+    from_port   = 465
+    to_port     = 465
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "application port range"
+    from_port   = 3000
+    to_port     = 10000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Simple Mail Transfer Protocol (SMTP) port for email notifications"
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubernetes API server (K8s)"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
