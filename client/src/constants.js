5de3f82a0e9a1fb3b1f124633e649f0df5416ae4
Ali Sameed <alisameed.cs@gmail.com>
2026-02-11 04:39:38 +0500

    fix: add redirects configuration in netlify.toml and update BASE_URL definition in constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1/";
+export const BASE_URL = import.meta.env.PROD
+  ? "/api/v1"
+  : import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1/";
 
