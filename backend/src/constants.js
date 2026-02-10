3c0df1acdc3cc76b8a35232641b179f08cd2a00a
Ali Sameed <alisameed.cs@gmail.com>
2026-02-11 03:33:19 +0500

    fix: update cookie options to enable secure flag and set sameSite to None

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -3,7 +3,7 @@ export const DB_NAME = "PlaybackSpace-DB";
 // setting cookies options
 export const OPTIONS = {
   httpOnly: true,
-  //secure: true,
-  sameSite: "Strict",
+  secure: true,
+  sameSite: "None",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
